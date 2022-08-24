f2d93cffe473a9b094431dc757ba76bce945e6d7
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-08-24 17:04:06 -0300

    requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,10 @@ function compareTrue(cozinha, banheiro) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = ((base * height) / 2);
+  return area;
+
 }
 
 // Desafio 3
